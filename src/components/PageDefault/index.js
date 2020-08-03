614fe6629454c47f21fef1a38b3eb3f95ebe7e10
Anry Dias <dev.anry@icloud.com>
2020-08-02 21:00:50 -0300

    Style PageDefault

--- src/components/PageDefault/index.js
+++ src/components/PageDefault/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import Menu from '../Menu';
 import Footer from '../Footer';
+import styled from 'styled-components';
 
-function PageDefault({  children    })  {
+const Main = styled.main`
+background-color: var(--cinza);
+color: var(--white);
+flex: 1;
+padding-top: 50px;
+padding-left: 5%;
+padding-right: 5%;
+padding-bottom: 50px
+`;
+
+
+function PageDefault({children})  {
     return(
-        <div>
+        <>
             <Menu />
-                {children}
+                <Main>
+                    {children}
+                </Main>
             <Footer />
-        </div>
+        </>
     )
 }
 
